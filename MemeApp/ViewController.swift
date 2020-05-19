1f907cdb37063996648f8384bcf7a82d239aa41d
arif dinulsyahputra <arif.putra@agenkan.co.id>
2020-05-19 16:51:13 +0700

    Initial Commit

---
+++ MemeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeApp
+//
+//  Created by On The Way on 19/05/20.
+//  Copyright © 2020 Dinulsyah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
